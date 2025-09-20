dee151ba1bd2d60efa3211a8f641caf2281f0ebd
Olivia <olivia.agostina.garcia@epet14.edu.ar>
2025-09-20 14:56:35 -0300

    merge: auth incompleto + main

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { login, register, forgotPassword, resetPassword } from '../controllers/auth.controller';
+import { requireAuth } from '../middleware/auth';
 
 const router = Router();
 
@@ -7,5 +8,6 @@ router.post('/login', login);
 router.post('/register', register);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
+router.post('/logout', requireAuth ); //
 
 export default router;
