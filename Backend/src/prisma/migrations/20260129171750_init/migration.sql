d5c02fd6f846080c4e3edf4a9678bdf1629c90cd
Juani <juani.arrigoni@gmail.com>
2026-01-30 18:19:38 -0300

    fix:

---
+++ Backend/src/prisma/migrations/20260129171750_init/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "public"."FORMULA_nombre_key";
+
+-- DropIndex
+DROP INDEX "public"."INSUMOS_nombre_key";
+
+-- DropIndex
+DROP INDEX "public"."PRODUCTOS_nombreComercial_key";
